5c86a47e2afbe5e49331ac38d5c7fe19dca52ad0
Caio Martins <icaiomarxm@gmail.com>
2025-06-07 17:44:21 -0400

    feat: Add courses table and model

--- 01_course_platform/src/persistence/models.py
+++ 01_course_platform/src/persistence/models.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
 
 class UserBase(SQLModel):
@@ -12,3 +12,16 @@ class User(UserBase, table=True):
     id: int | None = Field(primary_key=True, default=None)
     email: str = Field(unique=True, index=True)
     full_name: str | None = None
+
+    courses_instructed: list["Course"] = Relationship(back_populates="instructor")
+
+
+class Course(SQLModel, table=True):
+    __tablename__ = "courses"  # type: ignore
+
+    id: int | None = Field(primary_key=True, default=None)
+    name: str
+    description: str
+
+    instructor_id: int = Field(foreign_key="users.id")
+    instructor: User = Relationship(back_populates="courses_instructed")
