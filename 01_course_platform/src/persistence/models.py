cc639bec4dbd3bc5385bfcea7ee8d171ac90787b
Caio Martins <icaiomarxm@gmail.com>
2025-06-08 12:37:14 -0400

    feat: Add Enrollment table and model

--- 01_course_platform/src/persistence/models.py
+++ 01_course_platform/src/persistence/models.py
@@ -1,4 +1,24 @@
+from datetime import datetime
+from enum import Enum
+
+from sqlalchemy import null
 from sqlmodel import Field, Relationship, SQLModel
+
+
+class EnrollmentStatus(str, Enum):
+    ACTIVE = "active"
+    PAUSED = "paused"
+    COMPLETE = "complete"
+
+
+class Enrollment(SQLModel, table=True):
+    __tablename__ = "enrollments"  # type: ignore
+
+    user_id: int = Field(foreign_key="users.id", primary_key=True)
+    course_id: int = Field(foreign_key="courses.id", primary_key=True)
+
+    start_date: datetime = Field(default_factory=datetime.now)
+    status: EnrollmentStatus
 
 
 class UserBase(SQLModel):
@@ -15,6 +35,10 @@ class User(UserBase, table=True):
 
     courses_instructed: list["Course"] = Relationship(back_populates="instructor")
 
+    courses_enrolled: list["Course"] = Relationship(
+        back_populates="students", link_model=Enrollment
+    )
+
 
 class Course(SQLModel, table=True):
     __tablename__ = "courses"  # type: ignore
@@ -25,3 +49,7 @@ class Course(SQLModel, table=True):
 
     instructor_id: int = Field(foreign_key="users.id")
     instructor: User = Relationship(back_populates="courses_instructed")
+
+    students: list[User] = Relationship(
+        back_populates="courses_enrolled", link_model=Enrollment
+    )
